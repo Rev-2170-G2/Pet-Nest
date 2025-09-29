b48e9572d9ce09997f144c5b9c05e9a2aae403e7
Dean <190409058+abusooddean@users.noreply.github.com>
2025-09-29 15:20:42 -0400

    Mounted petRoutes

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 const { loggerMiddleware } = require('./util/logger');
 const { authenticateToken } = require('./util/jwt');
 
-const userRoutes = require('./routes/api/userRoutes');
+// const userRoutes = require('./routes/api/userRoutes');
+const petRoutes = require('./routes/api/petRoutes');
 
 const app = express();
 
@@ -11,11 +12,11 @@ app.use(bodyParser.json());
 app.use(loggerMiddleware);
 
 // can change /api to something else if desired
-app.use('/api/users', userRoutes);
+// app.use('/api/users', userRoutes);
+app.use('/api/pets', petRoutes);
 
 // example of middleware usage on a suite of routes
 // app.use('/api/route', authenticateToken, someRoutes);
-
 
 // for testing purposes only 
 app.get('/', (req, res) => { 
