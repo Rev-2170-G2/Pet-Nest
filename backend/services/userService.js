7909601814600ed33456f65e49f01df3ecc6df16
Heather Pfeiffer <heather.m.pfeiffer@outlook.com>
2025-09-29 16:46:48 -0400

    servive layer
    services for register and login added

---
+++ backend/services/userService.js
@@ -0,0 +1,45 @@
+const userDAO = require('../repository/userDAO');
+const { isValidUsernamePasswordAndEmail, createFormattedUserProfile } = require('../util/userValidation');
+const { logger }  = require('../util/logger');
+const bcrypt = require('bcrypt'); 
+
+
+async function registerUser (user) {
+    const { username, password, fullName, email } = user;
+
+    try {
+        if(user && await isValidUsernamePasswordAndEmail(username, password, email)) {
+            const formattedUser = await createFormattedUserProfile(username, password, fullName, email);
+            const data = await userDAO.registerUser(formattedUser);
+            logger.info({message: `Result data from registerUser in userService: ${data}`})
+            return formattedUser;
+        }
+    } catch (error) {
+        console.error(`Error in registerUser in userService: ${error}`);
+    }
+    return null;
+}
+
+
+const validateLogin = async (username, password) => {
+    try {
+        const user = await userDAO.getUserByUsername(username);
+        const isValid = !user ? null : await bcrypt.compare(password, user.password);
+
+        if (user && isValid) {
+            logger.info(`Returned data from validateLogin in userService: ${JSON.stringify(user)}`);
+            return user;
+        } else {
+            logger.warn(`Attempt to validate username and/or password failed for: ${JSON.stringify(username)}`);         
+        }
+    } catch (err) {
+        logger.error(`Error in validateLogin in userService: ${err}`); 
+    }
+    return null;
+}
+
+
+module.exports = {
+    registerUser,
+    validateLogin
+}
