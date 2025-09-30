6f9ee281908cc6daace1cc8eae502eda287eac62
Dean <190409058+abusooddean@users.noreply.github.com>
2025-09-30 10:12:52 -0400

    Move request validation logic to util

---
+++ backend/util/petValidation.js
@@ -0,0 +1,13 @@
+//check if pet attributes are not empty
+function validatePetData(req, res, next){
+    const {type, name, description, services} = req.body.pet;
+
+    if(!type || !name || !description || !services){
+        return res.status(400).json({message: "Missing fields: ", data: req.body.pet});
+    }
+    next();
+}
+
+module.exports = {
+    validatePetData,
+}
