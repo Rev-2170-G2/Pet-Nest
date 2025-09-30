b6dd076f2e7cf6dc7b41f9b7b9c8abee840fa761
Heather Pfeiffer <heather.m.pfeiffer@outlook.com>
2025-09-29 21:52:08 -0400

    separate route added for administrators who would like to register a new administrator that has additional middleware authenticateToken added to validate as admin

--- backend/routes/api/userRoutes.js
+++ backend/routes/api/userRoutes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../../controllers/userController');
+const { authenticateToken } = require('../../util/jwt');
 
 router.post('/login', userController.login);
 
 router.post('/register', userController.registerUser);
+
+router.post('admin/register', authenticateToken, userController.registerUser);
 
 
 module.exports = router;
