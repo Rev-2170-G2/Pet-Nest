826607dfd80fd104ee623baab53095fbd51b6658
Dean <190409058+abusooddean@users.noreply.github.com>
2025-10-01 12:03:34 -0400

    Update petValidation to prevent empty request data

--- backend/util/petValidation.js
+++ backend/util/petValidation.js
@@ -1,10 +1,16 @@
 //check if pet attributes are not empty
-function validatePetData(req, res, next){
+function validatePetData(req, res, next) {
     const {type, name, description, services} = req.body;
 
-    if(!type || !name || !description || !services){
-        return res.status(400).json({message: "Missing fields: ", data: req.body});
+    const typeResult = type && type.length > 0;
+    const nameResult = name && name.length > 0;
+    const descResult = description && description.length > 0;
+    const servicesResult = services && services.length > 0;
+
+    if (!(typeResult && nameResult && descResult && servicesResult)) {
+        return res.status(400).json({ message: "Missing or empty fields", data: req.body });
     }
+
     next();
 }
 
