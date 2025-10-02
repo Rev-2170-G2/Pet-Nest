258722d6c148c8a492723e910498dab75af6dc5f
Heather Pfeiffer <heather.m.pfeiffer@outlook.com>
2025-10-02 16:50:46 -0400

    logger fixed

--- backend/util/logger.js
+++ backend/util/logger.js
@@ -14,24 +14,6 @@ const logger = createLogger({
     ],
 });
 
-// const { createLogger, transports, format } = require('winston');
-// const { combine, timestamp, prettyPrint } = format; 
-
-// const logger = createLogger({
-//     format: combine(
-//         timestamp(),
-//         prettyPrint()
-//     ),
-//     transports: [
-//         new (transports.Console)({ level: 'info' }),
-//         new (transports.File)({
-//             name: 'info-file',
-//             filename: 'logs/filelog-info.log', // logs/item.log
-//             level: 'info'
-//         }),
-//     ]
-// });
-
 function loggerMiddleware(req, res, next) {
     logger.info(`Incoming ${req.method} : ${req.url}`);
     next();
