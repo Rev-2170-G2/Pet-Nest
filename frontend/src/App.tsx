3baed3f2c4788244044dc9ca224e91acaf56b6c6
ChristopherThorne714 <christhorne714@gmail.com>
2025-10-06 11:26:07 -0400

    scaffolded routes with react-router-dom and created a placeholder homepage route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home/Home';
 import './App.css'
 
 function App() {
@@ -8,7 +8,11 @@ function App() {
 
   return (
     <>
-      <div>
+
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+    </Routes>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +31,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
