2602ed4975c3eb3b94e1c49dbebf6a1c73f5a499
Heather Pfeiffer <heather.m.pfeiffer@outlook.com>
2025-10-06 13:15:31 -0400

    deleted comments

--- backend/routes/api/eventRoutes.js
+++ backend/routes/api/eventRoutes.js
@@ -6,7 +6,7 @@ const eventController = require('../../controllers/eventController');
 
 router.post('/', authenticateToken, eventController.PostEvent);
 
-router.get('/user', eventController.GetEventsByUser); //more specific routes first due to how Express reads routes 
+router.get('/user', eventController.GetEventsByUser); 
 
 router.get('/:id', eventController.GetEventById);
 
