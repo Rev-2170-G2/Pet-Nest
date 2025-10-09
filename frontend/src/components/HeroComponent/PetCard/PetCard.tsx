33be482ae7567cc10b658e2cea3b17e03ac86190
Heather Pfeiffer <heather.m.pfeiffer@outlook.com>
2025-10-09 16:15:27 -0400

    added PetCard component that uses axios to fetch pet data to send to CardDisplay component
    this pet card will grab all PETS and display the name, location, description and a button to send to pet profile for more information and send request for services

--- frontend/src/components/HeroComponent/PetCard/PetCard.tsx
+++ frontend/src/components/HeroComponent/PetCard/PetCard.tsx
@@ -1,35 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material';
 import './styles.css'
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import axios from 'axios';
 
-const cards = Array(4).fill(1);
+interface Pet {
+    entity: string,
+    type: string,
+    photos: string,
+    name: string,
+    location: string,
+    description: string
+}
 
 function PetCard() {
+    const [pets, setPets] = useState<Pet[]>([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchPets = async () => {
+            try {
+                const response = await axios.get("http://localhost:3000/api/pets")
+                console.log(response.data)
+                setPets(response.data.data.Items)
+            } catch (error) {
+                console.error("Error fetching pets: ", error)
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchPets()
+    }, [])
+
+    if (loading) return <p>Loading...</p>
 
   return (
-    <div className="petcard-containter">
-        {cards.map((_,index) => (
-            <div key={index}>
-                <Card sx={{ maxWidth: 250 }}>
-                <CardMedia
-                    sx={{ height: 140 }}
-                    image="https://cdn.pixabay.com/photo/2016/09/07/23/10/cat-1652880_1280.jpg"
-                    title="green iguana"
+    <div className="petcard-container">
+        {pets.map((pet, index) => (
+            <div key={index} className="pet-card">
+                <Card className="card-root">
+                <CardMedia className="card-media"
+                    image={pet.photos}
+                    title={pet.entity + pet.type}
                 />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
-                    Jedi Master Cat
+                    {pet.name}
+                    </Typography>
+                    <Typography variant="body2" sx={{ color: 'text.primary' }}>
+                    {"Location: " + pet.location}
                     </Typography>
                     <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                    Known across the galaxy, Jedi Master Cat Forhire is as cunning as she is powerful.
+                    {pet.description}
                     </Typography>
                 </CardContent>
-                <CardActions>
+                <CardActions className="card-actions">
                     <Button size="small">Pick Me</Button>
                 </CardActions>
                 </Card>
