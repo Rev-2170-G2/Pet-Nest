b5d703677ec138b96f2d4f1885cc3a202ef99169
Heather Pfeiffer <heather.m.pfeiffer@outlook.com>
2025-10-09 12:51:36 -0400

    added HeroSection to Home to be rendered by App

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import HeroSection from '../HeroComponent/HeroSection/HeroSection'
+import PetCard from '../HeroComponent/PetCard/PetCard'
 
 type Props = {}
 
 export default function Home({}: Props) {
   return (
     <>
-        <div>Home</div>
+        <HeroSection />
     </>
   )
 }
