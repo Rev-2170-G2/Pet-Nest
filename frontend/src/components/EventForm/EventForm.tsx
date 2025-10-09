8cc2dfca2f362f70039887af55fdb4d940f4ebc6
ChristopherThorne714 <christhorne714@gmail.com>
2025-10-09 09:09:57 -0400

    scaffolding

---
+++ frontend/src/components/EventForm/EventForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Form from 'react-bootstrap/Form'
+import FormLabel from 'react-bootstrap/FormLabel'
+import Button from 'react-bootstrap/Button';
+
+export default function EventForm() {
+  return (
+    <>
+    </>
+  )
+}
