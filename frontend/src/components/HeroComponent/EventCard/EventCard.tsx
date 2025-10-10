9e794fc3026f770eb40d3389036d9f400849d589
Heather Pfeiffer <heather.m.pfeiffer@outlook.com>
2025-10-09 21:00:32 -0400

    event card added to display in hero section
    card layout is located in its own css file and similar layout as pet card, like pet card an axios call to sent to retrieve all events and only open events are displayed by default currently

--- frontend/src/components/HeroComponent/EventCard/EventCard.tsx
+++ frontend/src/components/HeroComponent/EventCard/EventCard.tsx
@@ -1,30 +1,62 @@
 import { Button, Card, CardActions, CardContent, Typography } from '@mui/material'
 import CardMedia from '@mui/material/CardMedia'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './styles.css'
+import axios from 'axios';
 
-const cards = Array(4).fill(1);
+interface Events {
+    entity: string,
+    photos: string,
+    status: string,
+    name: string,
+    location: string,
+    description: string
+}
 
 function EventCard() {
+    const [events, setEvents] = useState<Events[]>([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchEvents = async () => {
+            try {
+                const response = await axios.get("http://localhost:3000/api/events")
+                console.log(response.data)
+                setEvents(response.data.data)
+            } catch (error) {
+                console.log('Error fetching events: ' + error)
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchEvents()    
+    }, [])
+
+    if (loading) return <p>Loading...</p>
+
   return (
     <div className="eventcard-container">
-        {cards.map((_,index) => (
-            <div key={index}>
-                <Card sx={{ maxWidth: 250 }}>
-                    <CardMedia
-                        sx={{ height: 140 }}
-                        image="https://cdn.pixabay.com/photo/2017/04/07/12/38/camera-2210891_1280.jpg"
-                        title="animal movie"
+        {events
+            .filter(event => event.status === "open")
+            .map((event, index) => (
+              <div key={index} className="event-card">
+                <Card className="card-root">
+                    <CardMedia className="card-media"
+                        image={event.photos}
+                        title={event.entity}
                     />
                     <CardContent>
                         <Typography gutterBottom variant="h5" component="div">
-                        Homeward Bound VIII Casting Call
+                        {event.name}
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: 'text.primary' }}>
+                        {event.location}
                         </Typography>
                         <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                        Got talents pets? Looking for a variety animals to star and be extras for upcoming shoot of Homeward Bound VIII. No previous experience required.
+                        {event.description}
                         </Typography>
                     </CardContent>
-                    <CardActions>
+                    <CardActions className="card-actions">
                         <Button size="small">Click here to learn more</Button>
                     </CardActions>
                 </Card>
