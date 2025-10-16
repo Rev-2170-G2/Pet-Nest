764ecd4224f6ac96c1bf20e0b6b7c46895cb128f
Heather Pfeiffer <heather.m.pfeiffer@outlook.com>
2025-10-16 16:31:01 -0400

    added link to the Pet Nest icon in the naviation bar to head to the home, if click log out will redirect to home page, added dark and light mode themeprovider

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,16 +1,23 @@
 import "./NavBar.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import Login from "../Auth/Login/Login";
 import Register from "../Auth/Register/Register";
+import { IconButton } from "@mui/material";
+import { DarkMode } from "@mui/icons-material";
+import { ColorModeContext } from "../../context/ColorModeContext"
+
 
 function NavBar() {
+  const navigate = useNavigate();
   const { user, logout } = useContext(AuthContext);
   const [showLogin, setShowLogin] = useState(false);
   const [showRegister, setShowRegister] = useState(false);
+  const colorMode = useContext(ColorModeContext);
 
   const handleLogout = () => {
+    navigate('/', {replace: true});
     logout();
   };
 
@@ -18,13 +25,19 @@ function NavBar() {
     <>
       <nav className="navbar">
         <div className="navbar-left">
-          <div className="navbar-logo">PetNest</div>
+          <Link to="/" className="navbar-logo">PetNest</Link>
         </div>
 
         <div className="navbar-right">
+
           <Link to="/" className="nav-link">Home</Link>
           <Link to="/pets-events" className="nav-link">Pets & Events</Link> {/* Separate to Pets/Events page. just filler right now. */}
           {user && <Link to="/offers" className="nav-link">Offers</Link>}
+
+          {/* toggle between light and dark mode */}
+          <IconButton className="toggle-button" onClick={colorMode.toggleColorMode} color="inherit" >
+            <DarkMode />
+          </IconButton>
 
           {user ? (
             <>
