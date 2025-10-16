e8bbb5c43d737f2e1b89c8bafff79531f88be6ab
Dean <190409058+abusooddean@users.noreply.github.com>
2025-10-16 10:40:51 -0400

    Update text and styling

--- frontend/src/components/Admin/Admin.tsx
+++ frontend/src/components/Admin/Admin.tsx
@@ -38,40 +38,48 @@ const handleDeny = (id: string) =>
   if (loading) return <p>Loading...</p>;
 
 return (
-  <div className="eventcard-container">
-    {events
-    .filter(event => event.approved === null) // filter for all newly created events
-      .map((event, index) => (
-        <div key={index} className="event-card">
-          <Card className="card-root d-flex flex-column h-100">
-            <CardMedia
-              className="card-media"
-              image={event.photos}
-              title={event.entity}
-              sx={{ height: 140 }}
-            />
-            <CardContent className="flex-grow-1">
-              <Typography gutterBottom variant="h5" component="div">
-                {event.name}
-              </Typography>
-              <Typography variant="body2" sx={{ color: 'text.primary' }}>
-                {event.location}
-              </Typography>
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                {event.description}
-              </Typography>
-            </CardContent>
-            <CardActions className="d-flex justify-content-between mt-auto">
-              <Button size="small" color="success" onClick={() => handleApprove(event.id)}>
-                Approve
-              </Button>
-              <Button size="small" color="error" onClick={() => handleDeny(event.id)}>
-                Deny
-              </Button>
-            </CardActions>
-          </Card>
-        </div>
-      ))}
+  <div className="eventcard-container d-flex flex-column">
+
+    <Typography variant="h5" component="h1" className="mb-5">
+      Events awaiting approval:
+    </Typography>
+
+    <div className="d-flex flex-row flex-wrap gap-3">
+      {events
+        .filter(event => event.approved === null)
+        .map((event, index) => (
+          <div key={index} className="event-card">
+            <Card className="card-root d-flex flex-column h-100">
+              <CardMedia
+                className="card-media"
+                image={event.photos}
+                title={event.entity}
+                sx={{ height: 140 }}
+              />
+              <CardContent className="flex-grow-1">
+                <Typography gutterBottom variant="h5" component="div">
+                  {event.name}
+                </Typography>
+                <Typography variant="body2" sx={{ color: 'text.primary' }}>
+                  {event.location}
+                </Typography>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  {event.description}
+                </Typography>
+              </CardContent>
+              <CardActions className="d-flex justify-content-between mt-auto">
+                <Button size="small" color="success" onClick={() => handleApprove(event.id)}>
+                  Approve
+                </Button>
+                <Button size="small" color="error" onClick={() => handleDeny(event.id)}>
+                  Deny
+                </Button>
+              </CardActions>
+            </Card>
+          </div>
+        ))}
+    </div>
+
   </div>
-)
+);
 }
