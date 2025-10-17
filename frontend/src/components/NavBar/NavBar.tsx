86f09a568c75f4dfc9ed2e9e12cba342b3567f8e
Heather Pfeiffer <heather.m.pfeiffer@outlook.com>
2025-10-17 14:23:42 -0400

    added the toggle back and adapted css for toggle - fyi: might been adjustment

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -64,6 +64,10 @@ function NavBar({ onJoinClick }: NavBarProps) {
           </div>
         </div>
 
+        <IconButton className="toggle-button" onClick={colorMode.toggleColorMode}>
+          <DarkMode />
+        </IconButton>
+
         <div className="navbar-right">
           <Link to="/" className="nav-link" onClick={handleGoHome}>
             Home
