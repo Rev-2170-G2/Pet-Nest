bb55e3ea9ffd00e5d758df32752cc5becb7c165e
ChristopherThorne714 <christhorne714@gmail.com>
2025-10-18 21:38:55 -0400

    removed an accidental automatic import

--- frontend/src/components/Review/Review.tsx
+++ frontend/src/components/Review/Review.tsx
@@ -5,8 +5,6 @@ import { Pet } from '../../types/Pet';
 import axios from 'axios';
 import { useAuth } from '../../context/AuthContext';
 import { toast } from 'react-toastify';
-import { Margin } from '@mui/icons-material';
-
 interface ReviewProps {
   pet: Pet;
   onReviewSubmit: (review: { rating: number; reviewText: string; createdAt: string }) => void;
